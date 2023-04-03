6351cb75669a21e0aa15e112e7802e48c57dea2b
Tranloi2k <tranloi20001007@gmail.com>
2023-04-03 14:59:32 +0700

    fix api url

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,14 +1,14 @@
 export const config = {
   "dev": {
-    "username": process.env.POSTGRESS_USERNAME,
-    "password": process.env.POSTGRESS_PASSWORD,
-    "database": process.env.POSTGRESS_DATABASE,
-    "host": process.env.POSTGRESS_HOST,
+    "username": "postgres",
+    "password": "Tranloi1",
+    "database": "udagramdatabase",
+    "host": "udagramdatabase.csoas8gzch40.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
-    "aws_region": process.env.AWS_REGION,
+    "aws_region": "us-east-1",
     "aws_profile": "default",
     "aws_media_bucket": process.env.AWS_MEDIA_BUCKET,
-    "url": process.env.FE_URL
+    "url": "a99099a184fdd4726a95203dba879ec4-775028303.us-east-1.elb.amazonaws.com:8000"
   },
   "jwt": {
     "secret": process.env.JWT_SECRET
