8e21287156350a4e9c793ca51d1d4700973ff48a
Tranloi2k <tranloi20001007@gmail.com>
2023-04-03 15:25:06 +0700

    fix api url

--- src/config/config.ts
+++ src/config/config.ts
@@ -8,7 +8,7 @@ export const config = {
     "aws_region": "us-east-1",
     "aws_profile": "default",
     "aws_media_bucket": process.env.AWS_MEDIA_BUCKET,
-    "url": "a99099a184fdd4726a95203dba879ec4-775028303.us-east-1.elb.amazonaws.com:8000"
+    "url": process.env.FE_URL
   },
   "jwt": {
     "secret": process.env.JWT_SECRET
